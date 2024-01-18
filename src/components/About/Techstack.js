1a5b891d82f2bbb96f7d59057f8ce7a2e4feb2a7
Phu Nguyen <phutom0412panda@gmail.com>
2024-01-18 17:41:15 -0500

    Update techstack and toolstack to make flipcard

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,66 +1,53 @@
-import React from "react";
-import { Col, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Col, Row } from 'react-bootstrap';
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
+  DiJavascript1, DiReact, DiNodejs, DiPython, DiGit, DiJava,
+} from 'react-icons/di';
 import {
-  SiRedis,
-  SiMysql,
-  SiApacheairflow,
-  SiApachespark,
-  SiPostgresql,
-  SiApachehadoop,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+  SiRedis, SiMysql, SiApacheairflow, SiApachespark, SiPostgresql, SiApachehadoop,
+} from 'react-icons/si';
+import { TbBrandGolang } from 'react-icons/tb';
+
+// Array of objects containing each technology's icon component and name
+const technologies = [
+  { IconComponent: DiPython, name: 'Python' },
+  { IconComponent: DiJava, name: 'Java' },
+  { IconComponent: DiJavascript1, name: 'JavaScript' },
+  { IconComponent: DiReact, name: 'React' },
+  { IconComponent: DiNodejs, name: 'Nodejs' },
+  { IconComponent: DiGit, name: 'Git' },
+  { IconComponent: TbBrandGolang, name: 'Golang' },
+  { IconComponent: SiApachespark, name: 'Apache Spark' },
+  { IconComponent: SiApacheairflow, name: 'Apache Airflow' },
+  { IconComponent: SiRedis, name: 'Redis' },
+  { IconComponent: SiMysql, name: 'MySQL' },
+  { IconComponent: SiPostgresql, name: 'PostgreSQL' },
+  { IconComponent: SiApachehadoop, name: 'Apache Hadoop' },
+];
 
 function Techstack() {
+  const [flipped, setFlipped] = useState({});
+
+  const toggleFlip = (index) => {
+    setFlipped(prev => ({ ...prev, [index]: !prev[index] }));
+  };
+
   return (
-    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJava />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachespark/>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApacheairflow />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiApachehadoop/>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiMysql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
+    <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+      {technologies.map(({ IconComponent, name }, index) => (
+        <Col key={index} xs={4} md={2} className="tech-icons">
+          <div className={`flip-card ${flipped[index] ? 'flipped' : ''}`} onClick={() => toggleFlip(index)}>
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <IconComponent title={name} />
+              </div>
+              <div className="flip-card-back">
+                <p>{name}</p>
+              </div>
+            </div>
+          </div>
+        </Col>
+      ))}
     </Row>
   );
 }
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
@@ -8,24 +8,38 @@ import {
   SiLinux,
 } from "react-icons/si";
 
+// Array of objects containing each tool's icon component and name
+const tools = [
+  { IconComponent: SiLinux, name: 'Linux' },
+  { IconComponent: SiVisualstudiocode, name: 'VS Code' },
+  { IconComponent: SiPostman, name: 'Postman' },
+  { IconComponent: SiAndroidstudio, name: 'Android Studio' },
+  { IconComponent: SiSlack, name: 'Slack' },
+];
+
 function Toolstack() {
+  const [flipped, setFlipped] = useState({});
+
+  const toggleFlip = (index) => {
+    setFlipped(prev => ({ ...prev, [index]: !prev[index] }));
+  };
+
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAndroidstudio />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col>
+      {tools.map(({ IconComponent, name }, index) => (
+        <Col key={index} xs={4} md={2} className="tech-icons">
+          <div className={`flip-card ${flipped[index] ? 'flipped' : ''}`} onClick={() => toggleFlip(index)}>
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <IconComponent title={name} />
+              </div>
+              <div className="flip-card-back">
+                <p>{name}</p>
+              </div>
+            </div>
+          </div>
+        </Col>
+      ))}
     </Row>
   );
 }
